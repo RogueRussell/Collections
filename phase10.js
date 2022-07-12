99a62ede9a884272da86e686a7a27e5cc9129dd3
RogueRussell <100233290+RogueRussell@users.noreply.github.com>
2022-07-12 14:15:03 -0600

    Create phase10.js

---
+++ phase10.js
@@ -0,0 +1,16 @@
+import ScoreboardView from "./resources/phase10score/ScoreboardView.js";
+
+let playerOneScore = 0;
+let playerTwoScore = 0;
+const root = document.querySelector("#app");
+const view = new ScoreboardView(root, "Nikki", "Chase", (player, direction) => {
+	const difference = direction === "minus" ? -1 : 1;
+
+	if (player === "one") {
+		playerOneScore = Math.max(playerOneScore + difference, 0);
+	} else {
+		playerTwoScore = Math.max(playerTwoScore + difference, 0);
+	}
+
+	view.update(playerOneScore, playerTwoScore);
+});
